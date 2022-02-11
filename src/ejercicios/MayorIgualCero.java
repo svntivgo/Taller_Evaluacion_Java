c8be09e07f9a71d2a725ebfdfdee9cdfce6d866f
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-11 17:35:15 -0500

    Ejercicio 7 input por ventana

--- src/ejercicios/MayorIgualCero.java
+++ src/ejercicios/MayorIgualCero.java
@@ -1,18 +1,18 @@
 package ejercicios;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class MayorIgualCero {
     public void comprobar () {
-        Scanner datoIngresado = new Scanner(System.in);
+        String datoIngresado = JOptionPane.showInputDialog("Ingrese un número mayor a cero");
         System.out.println("Este programa te indica si el primer número ingresado es mayor o igual a cero");
 
-        System.out.println("Ingrese un número mayor a 0");
-        Integer numero = Integer.parseInt(datoIngresado.nextLine());
+        Integer numero = Integer.parseInt(datoIngresado);
 
         do {
-            System.out.println(numero +" no es mayor a cero, ingrese nuevamente un número");
-            numero = Integer.parseInt(datoIngresado.nextLine());
+            datoIngresado = JOptionPane.showInputDialog(numero +" no es mayor a cero, ingrese un número");
+            numero = Integer.parseInt(datoIngresado);
         } while (numero < 0);
 
         System.out.println(numero +" es igual o mayor a cero");
