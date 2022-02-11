ce39cd318d2278ac224f8c49568b389f4aa51d19
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-11 18:14:21 -0500

    - scanner, +input dialog, mensaje corregido

--- src/ejercicios/AreaCirculo.java
+++ src/ejercicios/AreaCirculo.java
@@ -1,19 +1,22 @@
 package ejercicios;
 
-import java.util.Scanner;
+import javax.swing.*;
 
 public class AreaCirculo {
     /**
      * Calcula el área de un círculo con su radio
      */
     public void calcular () {
-        Scanner numero = new Scanner(System.in);
+        String numero = JOptionPane.showInputDialog("Ingrese el radio en metros");
         System.out.println("Este programa indica el área de un círculo dado su radio en metros");
 
-        System.out.println("Ingrese el radio en metros");
-        double radio = Double.parseDouble(numero.nextLine());
+        double radio = Double.parseDouble(numero);
 
         double areaCirculo = Math.PI * Math.pow(radio, 2);
-        System.out.println("El radio del círculo es: "+ String.format("%.2f", areaCirculo) +" metros cuadrados");
+
+        JOptionPane.showMessageDialog(
+                null,
+                "El area del círculo es: "+ String.format("%.2f", areaCirculo) +" metros cuadrados"
+        );
     }
 }
