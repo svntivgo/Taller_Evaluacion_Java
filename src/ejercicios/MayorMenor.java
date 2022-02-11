3c36123c10dfc8514e93b3ba55d03121014f51e1
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-11 00:54:43 -0500

    ingreso de datos por consola

--- src/ejercicios/MayorMenor.java
+++ src/ejercicios/MayorMenor.java
@@ -1,16 +1,27 @@
 package ejercicios;
 
-public class MayorMenor {
-    private int num_a = 5;
-    private int num_b = 5;
+import java.util.Scanner;
 
+public class MayorMenor {
+    /**
+     * Compara el primer número ingresado con el segundo
+     */
     public void comparador(){
-        if (num_a > num_b) {
-            System.out.println("Es mayor");
-        } else if (num_a < num_b) {
-            System.out.println("es menor");
+        Scanner numero = new Scanner(System.in);
+        System.out.println("Este programa te indica si el primer número ingresado es mayor al segundo");
+
+        System.out.println("Ingrese el primer número número");
+        Integer numero_a = Integer.parseInt(numero.nextLine());;
+
+        System.out.println("Ingrese el segundo número número");
+        Integer numero_b = Integer.parseInt(numero.nextLine());
+
+        if (numero_a > numero_b) {
+            System.out.println(numero_a +" es mayor que "+ numero_b);
+        } else if (numero_a < numero_b) {
+            System.out.println(numero_a +" es menor que "+ numero_b);
         } else {
-            System.out.println("son iguales");
+            System.out.println(numero_a +" es igual a "+ numero_b);
         }
     }
 }
--- src/main.java
+++ src/main.java
@@ -2,7 +2,10 @@ import ejercicios.MayorMenor;
 
 public class main {
     public static void main(String[] args) {
-        MayorMenor ejercicio1 = new MayorMenor();
-        ejercicio1.comparador();
+        /**
+         * Este ejercicio contempla los puntos 1 y 2 del taller
+         */
+//        MayorMenor ejercicio1 = new MayorMenor();
+//        ejercicio1.comparador();
     }
 }
