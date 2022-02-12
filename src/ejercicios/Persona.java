b1ace92547efda694cc82d02f4b244b8db3b96c7
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-12 00:59:40 -0500

    ejercicio 16 creo objeto, creo calcularIMC

---
+++ src/ejercicios/Persona.java
@@ -0,0 +1,50 @@
+package ejercicios;
+
+public class Persona {
+    private final String hombre = "H";
+    private final String mujer = "M";
+    private String nombre, dni, sexo;
+    private Integer edad, peso, altura, pesoBajo, pesoIdeal, pesoAlto;
+
+    public Persona () {
+        this.nombre = "";
+        this.edad = 0;
+        this.dni = null;
+        this.sexo = "";
+        this.peso = 0;
+        this.altura = 0;
+    }
+
+    public void constructorBasico (String nombre, Integer edad, String sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public void constructorCompleto (String nombre, Integer edad, String dni, String sexo, Integer peso, Integer altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public Integer calcularIMC () {
+        Double alturaEnMetros = (this.altura/100.0);
+        Double imc = (this.peso / Math.pow(alturaEnMetros, 2));
+        Integer resultado = null;
+
+        if (imc < 20) {
+            resultado = -1;
+        } else if (imc >= 20 && imc <= 25) {
+            resultado = 0;
+        } else {
+            resultado = 1;
+        }
+
+        return resultado;
+    }
+
+
+}
--- src/main.java
+++ src/main.java
@@ -92,5 +92,12 @@ public class main {
         MenuCinema ejercicio15 = new MenuCinema();
 //        ejercicio15.mostrar();
 
+        /**
+         * Este ejercicio contemple el punto 16 del taller
+         */
+        Persona ejercicio16 = new Persona();
+        ejercicio16.constructorCompleto("Santiago",  29, "1088297271", "H", 73, 183);
+        System.out.println(ejercicio16.calcularIMC());
+
     }
 }
