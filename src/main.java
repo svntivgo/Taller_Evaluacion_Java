2c12dec6c7889572507e0a509ff41ac9aeb33651
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-12 17:20:10 -0500

    comentarios corregidos

--- src/main.java
+++ src/main.java
@@ -21,84 +21,89 @@ public class main {
 //        ejercicio3.calcular();
 
         /**
-         * Este ejercicio contemple el punto 4 del taller
+         * Este ejercicio contempla el punto 4 del taller
          */
         PrecioIVA ejercicio4 = new PrecioIVA();
 //        ejercicio4.calcular();
 
         /**
-         * Este ejercicio contemple el punto 5 del taller
+         * Este ejercicio contempla el punto 5 del taller
          */
         ParImpar ejercicio5 = new ParImpar();
 //        ejercicio5.mostrarWhile();
 
         /**
-         * Este ejercicio contemple el punto 6 del taller
+         * Este ejercicio contempla el punto 6 del taller
          */
         ParImpar ejercicio6 = new ParImpar();
 //        ejercicio6.mostrarFor();
 
         /**
-         * Este ejercicio contemple el punto 7 del taller
+         * Este ejercicio contempla el punto 7 del taller
          */
         MayorIgualCero ejercicio7 = new MayorIgualCero();
 //        ejercicio7.comprobar();
 
         /**
-         * Este ejercicio contemple el punto 8 del taller
+         * Este ejercicio contempla el punto 8 del taller
          */
         DiaLaboral ejercicio8 = new DiaLaboral();
 //        ejercicio8.comprobar();
 
         /**
-         * Este ejercicio contemple el punto 9 del taller
+         * Este ejercicio contempla el punto 9 del taller
          */
         Frase ejercicio9 = new Frase();
 //        ejercicio9.modificar();
 
         /**
-         * Este ejercicio contemple el punto 10 del taller
+         * Este ejercicio contempla el punto 10 del taller
          */
         Frase ejercicio10 = new Frase();
 //        ejercicio10.elimnarEspacios();
 
         /**
-         * Este ejercicio contemple el punto 11 del taller
+         * Este ejercicio contempla el punto 11 del taller
          */
         Frase ejercicio11 = new Frase();
 //        ejercicio11.contabilizar();
 
         /**
-         * Este ejercicio contemple el punto 12 del taller
+         * Este ejercicio contempla el punto 12 del taller
          */
         Frase ejercicio12 = new Frase();
 //        ejercicio12.comparar();
 
         /**
-         * Este ejercicio contemple el punto 13 del taller
+         * Este ejercicio contempla el punto 13 del taller
          */
         Fecha ejercicio13 = new Fecha();
 //        ejercicio13.consultar();
 
         /**
-         * Este ejercicio contemple el punto 14 del taller
+         * Este ejercicio contempla el punto 14 del taller
          */
         Contador ejercicio14 = new Contador();
 //        ejercicio14.contar();
 
         /**
-         * Este ejercicio contemple el punto 15 del taller
+         * Este ejercicio contempla el punto 15 del taller
          */
         MenuCinema ejercicio15 = new MenuCinema();
 //        ejercicio15.mostrar();
 
         /**
-         * Este ejercicio contemple el punto 16 del taller
+         * Este ejercicio contempla el punto 16 del taller
          */
         BienestarPersonal ejercicio16 = new BienestarPersonal();
 //        ejercicio16.personaCompleto();
 //        ejercicio16.personaBasico();
 //        ejercicio16.personaManual();
+
+        /**
+         * Este ejercicio contempla el punto 17 del taller
+         */
+        BienestarPersonal ejercicio17 = new BienestarPersonal();
 
     }
 }
